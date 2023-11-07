065ae2609fba4832d3d35ee611cd66f7848e2e8d
krystiansledz <krystiansledz419@gmail.com>
2023-11-07 20:17:03 +0100

    test CI/CD

--- Data/WeatherForecastService.cs
+++ Data/WeatherForecastService.cs
@@ -4,7 +4,7 @@ public class WeatherForecastService
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing in CI/CD"
     };
 
     public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
